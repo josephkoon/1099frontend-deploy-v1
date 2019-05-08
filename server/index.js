d328f857479369e14e9c3c4c4c50355aeca666f0
Joseph Koon <josephandrewkoon@gmail.com>
2019-05-07 19:48:13 -0700

    updates

--- server/index.js
+++ server/index.js
@@ -2,7 +2,19 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var https = require('https');
+var port = 10443;
+
+var p_key = fs.readFileSync('/home/bitnami/config/privkey.pem');
+var cert = fs.readFileSync('/home/bitnami/config/cert.pem')
+
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.listen(80);
+
+https.createServer({
+	key:p_key,
+	cert:cert
+}, app).listen(port, () => console.log('Listening on port', port));
+
+
 
